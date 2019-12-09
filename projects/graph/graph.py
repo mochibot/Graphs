3fb4ba550cf944f3a6b134d98e2e0365676cc2d3
mochibot <amusaur83@gmail.com>
2019-12-09 15:42:02 -0800

    tweaked exception message

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -27,7 +27,7 @@ class Graph:
                 raise KeyError
             self.vertices[v1].add(v2)
         except KeyError:
-            print(f'No vertex of {v1} or {v2}')
+            print(f'Either {v1} or {v2} is not a valid vertex')
             return
 
     def get_neighbors(self, vertex_id):
@@ -37,7 +37,7 @@ class Graph:
         try:
             return self.vertices[vertex_id]
         except KeyError:
-            print(f'No vertex of {vertex_id}')
+            print(f'Vertex {vertex_id} does not exist')
             return None
 
     def bft(self, starting_vertex):
